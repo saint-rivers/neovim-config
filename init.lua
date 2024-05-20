229a2cee0f504c9fa7bd3a3100d54a5cc7f20adc
dayan <eam.dayan@gmail.com>
2024-05-21 00:22:09 +0900

    added treesitter

--- init.lua
+++ init.lua
@@ -2,3 +2,9 @@ require("core.keymap")
 require("core.set")
 require("core.lazy")
 require("core.plugins")
+
+-- require("lspconfig").gopls.setup({
+--     on_attach = function()
+--         vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+--     end
+-- })
