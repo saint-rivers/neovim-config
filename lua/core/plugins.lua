fc446c1ced80ccf4297494ef513986e105cdd7a4
dayan <eam.dayan@gmail.com>
2024-05-21 01:51:41 +0900

    added aerials

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,10 +1,23 @@
+-- telescope config
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+-- set color
 vim.cmd("colorscheme kanagawa")
 
-
+-- enable mason
 require("mason").setup()
+
+-- enable aerial to view members in a file
+require("aerial").setup({
+  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+  on_attach = function(bufnr)
+    -- Jump forwards/backwards with '{' and '}'
+    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+  end,
+})
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
