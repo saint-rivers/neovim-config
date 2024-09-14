d2ec662a791cb0ba7dc295b756b4d983d70844c9
dayan <eam.dayan@gmail.com>
2024-09-14 12:54:11 +0900

    angular ls config

--- init.lua
+++ init.lua
@@ -1,1 +1,43 @@
 require("saintrivers")
+
+-- local project_library_path = "~/.local/share/nvim/mason/packages/angular-language-server/node_modules/@angular/language-server/"
+local ok, mason_registry = pcall(require, 'mason-registry')
+if not ok then
+    vim.notify 'mason-registry could not be loaded'
+    return
+end
+
+local angularls_path =
+    mason_registry.get_package('angular-language-server'):get_install_path()
+
+-- local cmd = {
+--     "ngserver",
+--     "--ngProbeLocations",
+--     angularls_path,
+--     "--tsProbeLocations",
+--     angularls_path ,
+--     "--stdio",
+-- }
+
+local cmd = {
+    'ngserver',
+    '--stdio',
+    '--tsProbeLocations',
+    table.concat({
+        angularls_path,
+        vim.uv.cwd(),
+    }, ','),
+    '--ngProbeLocations',
+    table.concat({
+        angularls_path .. '/node_modules/@angular/language-server',
+        vim.uv.cwd(),
+    }, ','),
+}
+
+require'lspconfig'.angularls.setup{
+    cmd = cmd,
+    on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = cmd
+    end
+}
+
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,9 +1,9 @@
 -- telescope config
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- set color
 vim.cmd("colorscheme kanagawa")
@@ -13,15 +13,47 @@ require("mason").setup()
 
 -- enable aerial to view members in a file
 require("aerial").setup({
-  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-  on_attach = function(bufnr)
-    -- Jump forwards/backwards with '{' and '}'
-    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-  end,
+	-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+	on_attach = function(bufnr)
+		-- Jump forwards/backwards with '{' and '}'
+		vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+		vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+	end,
 })
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 
 -- enable tabs for editor
 vim.opt.termguicolors = true
-require("bufferline").setup {}
+require("bufferline").setup({})
+
+-- code block indents
+require("ibl").setup()
+
+--
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"gopls",
+		--		"rust_analyzer",
+		"angularls",
+		"tsserver",
+		"tailwindcss",
+		"dockerls",
+		"docker_compose_language_service",
+		"clangd",
+	},
+})
+
+-- code formatter
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		javascript = { { "prettierd", "prettier" } },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+})
